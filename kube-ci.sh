317c97a9d76911bc6e475313e532a298d71ef84d
Julian <juliantellezmendez@gmail.com>
2022-08-26 21:03:17 +0100

    chore: simple loop to apply manifests

--- kube-ci.sh
+++ kube-ci.sh
@@ -5,11 +5,14 @@
     # GIT_REPO
     # KUBE_MANIFEST_PATH
 
+mkdir -p $GIT_DESTINATION
 cd $GIT_DESTINATION
-APPLY_PATH=$(pwd)/${GIT_DESTINATION}
 
+ssh-keyscan -H github.com >> /root/.ssh/known_hosts
 git clone $GIT_REPO
 
+
+APPLY_PATH=$(pwd)/${GIT_DESTINATION}
 while true;
 do
 echo -n "Applying ${APPLY_PATH}";
