b54e416b3fd69d70831c6d95b460ceef55cabffa
Julian <juliantellezmendez@gmail.com>
2022-08-27 00:48:55 +0100

    chore: add ssh keys

--- kube-ci.sh
+++ kube-ci.sh
@@ -4,28 +4,34 @@
 # GIT_DESTINATION
 # GIT_REPO
 # KUBE_MANIFEST_PATH
+# GIT_SSH_FOLDER
 
+## 1. Prepare Credentials
+mkdir -p /root/.ssh
+cp -R GIT_SSH_FOLDER /root/.ssh
+ssh-keyscan -H github.com >> /root/.ssh/known_hosts
+
+## 2. Clone repo
 mkdir -p $GIT_DESTINATION
 cd $GIT_DESTINATION
-
-ssh-keyscan -H github.com >> /root/.ssh/known_hosts
 git clone $GIT_REPO
 
 
+## 3. Pull and apply loop
 APPLY_PATH=$(pwd)/${KUBE_MANIFEST_PATH}
 while true;
 do
     echo -n "[ DATE ] $(date) \n";
-
+    
     echo -n "[ GITHUB ] Pulling latest \n";
     git pull
     echo -n "\n";
     echo -n "\n";
-
+    
     echo -n "[ K8s ] kubectl apply -k ${APPLY_PATH} \n";
     kubectl apply -k ${APPLY_PATH}
     echo -n "\n";
     echo -n "\n";
-
+    
     sleep 60;
 done
